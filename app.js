9f9ca969f51d96899062f4c0389e02acc52e2f6c
Dan Perata <djperata@aol.com>
2014-05-15 18:11:08 -0600

    added all languages to drop down menus and made code of language available to use for input

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var BeGlobal = require('node-beglobal');
+var _ = require('underscore')
 
 //initialize the BeGlobal API
 var beglobal = new BeGlobal.BeglobalAPI({
@@ -12,35 +13,56 @@ app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
-var languageArray= ["English", "Turkish","Spanish", "Bosnian"];
+
 
 app.get('/', function(req, res) {
-	res.render('index', {
-    languages: languageArray
-  });
-
-
-});
-
+	
 var allLanguages = beglobal.languages.all(function(err, results){
   if (err){
     return console.log(err);
   }
-  console.log(results);
+  // console.log(results);
+
+  var uniqueLanguages = _.chain(results).pluck('from').uniq(true, function(item){return item.name}).value()
+  // res.send(uniqueLanguages)
+  // res.send(results.map(function(item){
+    // return item.from.name
+  // }))
+  res.render('index', {
+    languages: uniqueLanguages
+  });
 })
+
+
+});
+
 
 var server = app.listen(5609, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
 
-
-beglobal.translations.translate(
-  {text: 'hello', from: 'eng', to: 'fra'},
-  function(err, results) {
-    if (err) {
-      return console.log(err);
-    }
-
-    console.log(results);
+app.post('/languages', function(req, res){
+  
+  var langCode = beglobal.languages.all(function(err, results){
+    if (err){
+    return console.log(err);
   }
-);
+  // var langFrom = filter
+
+  // var langTo
+
+
+  beglobal.translations.translate({
+    text: 'hello', 
+    from: 'en', 
+    to: 'fra'
+  },
+    function(err, results) {
+      if (err) {
+        return console.log(err);
+      }
+
+      console.log(results);
+    });
+});
+});  
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,19 @@
 $(document).on("ready", function(){
 	$('.dropdown-toggle').dropdown()
+	$('.dropdown li a').click(function(){
+
+      $(this).closest('.dropdown').find(".btn:first-child").text($(this).text())
+      	.val($(this).attr('data-code'));
+      	// console.log($(".from button").text($(this).text()))
+
+   });
+$('#form').on("submit", function(e) {
+	e.preventDefault();
+	$.post('/languages', {
+		from: $(".from button").val(),
+		to: $(".to button").val() 
+	})
 })
+
+})
+
